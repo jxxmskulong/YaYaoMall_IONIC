86b19bfd764a5ee0d91759282602ac2a4cad66d7
nieyue <278076304@qq.com>
2016-03-14 19:14:21 +0800

    2016 3 14

--- YaYaoMall/WebRoot/www/js/controllers.js
+++ YaYaoMall/WebRoot/www/js/controllers.js
@@ -1,6 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,$timeout) {
+    $scope.myActiveSlide = 0;
+    $scope.downRefresh=false;
+  $scope.list=[{id:100,age:30,name:"张三"}];
+   $scope.$watch(function(){
+   //alert('sh')
+        if(document.body.scrollTop+window.screen.height>document.body.scrollHeight)
+        {
+          //alert('sh')
+       /*alert(document.body.scrollTop);
+    alert(window.screen.height);
+    alert(document.body.scrollHeight); */ 
+          $scope.downRefresh=true;
+          var obj={id:101,age:30,name:"李四"};
+          //alert('bottom');
+              $timeout(function(){
+         //$scope.list.push(obj);
+         $scope.downRefresh=false;
+                
+              },5000);
+      }
+    }
+
+    );
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -30,4 +55,42 @@ angular.module('starter.controllers', [])
 .controller('CatCtrl', function($scope) {
  $scope.aaa="sdaf";
  alert("dfdf");
+})
+.controller('PersonCtrl', function($scope,$ionicModal) {
+  //alert($ionicModal.modal.show());
+  var urlR=["tab-register.html","tab-login.html"];
+  var a=3;
+  var urli="";
+  if(a>2){
+    urli=urlR[0];
+  }
+  console.log(urli);
+ $ionicModal.fromTemplateUrl(urli, {
+    scope: $scope,
+    animation: 'silde-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+     $scope.modal.show();
+   };
+  $scope.closeModal = function() {
+     $scope.modal.hide();
+   };
+
+   //当我们用完模型时，清除它！
+   $scope.$on('$destroy', function() {
+     $scope.modal.remove();
+   });
+   // 当隐藏模型时执行动作
+   $scope.$on('modal.hide', function() {
+     // 执行动作
+     //$scope.openModal();
+     alert('dsf')
+   });
+   // 当移动模型时执行动作
+   $scope.$on('modal.removed', function() {
+     // 执行动作
+     alert('removed')
+   });
 });
